858e2641c3ee892a918a5155f0f92914747a9a62
Sola <solanylander@gmail.com>
2018-06-10 10:06:54 +0100

    Refactor & Comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math, random, sys, fileinput
 
 inputs = fileinput.input()
-
+# Main Class
 class Main():
 
 	def __init__(self, inputs):	
@@ -16,14 +16,13 @@ class Main():
 		for line in inputs:
 			print_line = True
 			for char in line:
+				# Detects missing lead zeros
 				floating_point[0] = floating_point[1]
 				floating_point[1] = floating_point[2]
 				floating_point[2] = char
 				if floating_point[1] == '.' and floating_point[2].isdigit():
 					if not floating_point[0].isdigit():
-						sys.stderr.write("------------------\nErr with line\n")
-						sys.stderr.write(line)
-						sys.stderr.write("\nMissing Lead zero\n------------------\n")
+						self.print_error(line, "Missing lead zero")
 						del stack[len(stack) - 1]
 						print_line = False
 						break
@@ -31,37 +30,40 @@ class Main():
 				if char in open_braces:
 					stack.append(char)
 				elif char in close_braces:
+					# Incorrect Closing parentesis
 					if not(len(stack) == 0):
 						if (stack[len(stack) - 1] == "[" and char == "]") or (stack[len(stack) - 1] == "{" and char == "}"):
 							del stack[len(stack) - 1]
 						else:
-							sys.stderr.write("------------------\nErr with line\n")
-							sys.stderr.write(line)
-							sys.stderr.write("\nIncorrect closing parentesis\n------------------\n")
+							self.print_error(line, "Incorrect closing parentesis")
 							del stack[len(stack) - 1]
 							print_line = False
 							break
 				elif char in quotes:
+					# Incorrect Quotations ' instead of "
 					if char == "\'" and len(quote_queue) > 0:
 						if quote_queue[len(quote_queue) - 1] == "\'":
-							sys.stderr.write("------------------\nErr with line\n")
-							sys.stderr.write(line)
-							sys.stderr.write("Incorrect quotation marks\n------------------\n")		
+							self.print_error(line, "Incorrect Quotation Marks")	
+							print_line = False
+							break
+					# Nedted Quotations "string"example""
+					if char == "\"" and len(quote_queue) > 2:
+						if quote_queue[len(quote_queue) - 1] == "\"" and quote_queue[len(quote_queue) - 2] == "\"" and quote_queue[len(quote_queue) - 3] == "\"":
+							quote_queue.append(":")
+							self.print_error(line, "Nested Quotation Marks")
 							print_line = False
 							break
 
-					if char == "\"" and len(quote_queue) > 2:
-						if quote_queue[len(quote_queue) - 1] == "\"" and quote_queue[len(quote_queue) - 2] == "\"" and quote_queue[len(quote_queue) - 3] == "\"":
-							sys.stderr.write("------------------\nErr with line\n")
-							sys.stderr.write(line)
-							sys.stderr.write(quote_queue[len(quote_queue) - 1] +" " +quote_queue[len(quote_queue) - 2] + " "+ quote_queue[len(quote_queue) - 3])
-							sys.stderr.write("Nested Quotation Marks\n------------------\n")
-							print_line = False
-							quote_queue.append(":")
-							break
 					quote_queue.append(char)
+			# Print valid lines
 			if print_line:
 				print(line)
 
+
+	# Print error message to stderr
+	def print_error(self, line, error_msg):
+		sys.stderr.write("------------------\nErr with line\n")
+		sys.stderr.write(line)
+		sys.stderr.write("\n" + error_msg + "\n------------------\n")
 
 Main(inputs)
--- readme.txt
+++ readme.txt
@@ -5,3 +5,5 @@ Run in the console using
 python main.py colours_error.json
 or 
 python main.y colours.json
+
+All requirements satisfied & it also detects irregular paranthesis {/[
